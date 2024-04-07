1d039307d2a72c8392f5c9ad6b0da1a8ed32d6be
obeedril <obeedril@student.21-school.ru>
2024-04-07 16:22:12 +0300

    start

---
+++ src/main/java/com/example/nechto/NechtoApplication.java
@@ -0,0 +1,14 @@
+package com.example.nechto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class })
+public class NechtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NechtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nechto/NechtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nechto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NechtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
